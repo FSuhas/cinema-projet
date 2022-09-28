843be1c6562d7506214881b8053a08563be4089b
fabien suhas <fsuhas@gmx.com>
2022-09-28 15:39:02 +0200

    sweetalert add and config

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,16 +8,16 @@ class BookingsController < ApplicationController
     if @booking.save
       mail = BookingMailer.with(booking: @booking).booking_confirmation
       mail.deliver_now
-      redirect_to posts_path, notice: "Votre demande a bien été envoyée"
+      redirect_to posts_path, notice: "Candidature transmise avec succès"
     else
-      redirect_to posts_path, alert: "Vous avez déja postulé à cette offre !", status: :unprocessable_entity
+      redirect_to posts_path, alert: "Candidature déjà en cours...", status: :unprocessable_entity
     end
   end
 
   def destroy
     @booking = Booking.find(params[:id])
     if @booking.destroy
-      redirect_to archive_path, alert: "Votre demande a bien été supprimée"
+      redirect_to archive_path, alert: "Vous avez retiré votre candidature"
     end
   end
 
--- app/javascript/controllers/sweetalert_controller.js
+++ app/javascript/controllers/sweetalert_controller.js
@@ -5,13 +5,66 @@ import Swal from 'sweetalert2'
 export default class extends Controller {
   static values = {text: String}
   connect() {
-    Swal.fire({
-      icon: "success",
-      text: this.textValue,
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top',
       showConfirmButton: false,
-      timer: 4000
-    });
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+      Toast.fire({
+        icon: 'info',
+        title: this.textValue,
+      })
+  }
+
+  success() {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    if (this.event) {
+      Toast.fire({
+        icon: 'success',
+        title: 'Votre candidature a bien été envoyée'
+      })
+    }
+    else {
+      Toast.fire({
+        icon: 'error',
+        title: 'Vous avez déjà postulé à cette offre'
+      })
+    }
+  }
+
+  error() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: this.textValue,
+      confirmButtonText: 'Ok'
+    })
   };
 
-
+  warning() {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Warning!',
+      text: this.textValue,
+      confirmButtonText: 'Ok'
+    })
+  };
 }
