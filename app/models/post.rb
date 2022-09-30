f26dd8dbcebc66c8e64e4d176731342cff3fb255
fabien suhas <fsuhas@gmx.com>
2022-09-30 21:40:17 +0200

    start new filterbar

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,20 +2,20 @@ class Post < ApplicationRecord
 
   GENRE = %w[Homme Femme Autre]
   ROLE = (["Figurant", "Doublure", "Silhouette", "Rôle"])
-  AGE = (["18-90", "18-25", "25-35","35-45", "45-55", "55-65","65-75","75-90" ])
+  AGE = (["18-90", "18-25", "25-35","35-45", "45-55", "55-65","65-75","75-90"])
 
   has_many_attached :photos
   belongs_to :user
 
-  validates :titre, :descriptif, :date, presence: true
+  validates :titre, :descriptif, presence: true
   validates :genre, presence: true, inclusion: { in: GENRE }
-  validates :role, presence: true, inclusion: { in: ROLE }
+  validates :age, presence: true, inclusion: { in: AGE }
 
 
   include PgSearch::Model
 
   pg_search_scope :search_by_query,
-  against: [ :titre, :role, :ville, :departement, :lieux, :descriptif, :age ],
+  against: [ :departement, :descriptif, :titre ],
     using: {
     tsearch: { prefix: true }
   }
